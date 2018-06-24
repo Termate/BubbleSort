c472f8709db1e027fdec913ed067b077fd14bef9
Nik Fedorov <fedorovnu@yandex.ru>
2018-06-24 19:18:51 +0300

    second version

--- src/bubbleSort.js
+++ src/bubbleSort.js
@@ -1,17 +1,18 @@
-var arrX, arrayTo, inputText;
+var arrX, arrayTo, inputText, xmax, xmin, length;
 var tegs = [];
 var mass = [];
 var i = 0;
 var j = 0;
-
+var next = document.getElementById('next');
 inputText = document.getElementById('inputText');
+var position = document.getElementById('list');//нахождение элемента <div>
+
 
 inputText.onkeypress = function (e) {
         check(e);
     }
 
 function check(e) {
-    // var value = e.value;
     var key = e.keyCode || e.which;
     //метод fromCharCode возвращает элементарную строку, а не объект типа String.
     key = String.fromCharCode( key );
@@ -29,40 +30,23 @@ function check(e) {
        }
   }
 
-function toNumber(wx) {
-  for (var i=0; i<mass.length; i++) {
-    mass[i] = mass[i]*1;
-  }
-  // console.log(mass);
-  return mass;
-}
-
-
 // создает элемент <p> и добавляет его в <div> с id ='list'
 function addP(x) {
  for (var i = 0; i < x.length; i++) {
     var p = document.createElement('p');
     p.className = "pclass";
-    p.id = ('s'+x[i]);
     p.innerHTML = x[i];
-    // console.log(p.innerHTML);
     styleForm(x[i], p);
-    var position = document.getElementById('list');//нахождение элемента <div>
+
  }
- start()
+ toBlue()
  position.appendChild(p);//добавляем в <div> элемент <p>
  tegs = document.getElementsByTagName('p'); //назодим все элементы по тегу
- arrayTo = toNumber();
-// console.log(p)
+ length = tegs.length;
 }
 
 
-//при каждом создании элемента должна сробатывать функция
-function start(){
-  var quer = document.getElementsByClassName('pclass')
-   for (var i=0; i<quer.length; i++) {
-    quer[i].style.backgroundColor = "#c7240c";
-  };
+function toBlue(){
 
 setTimeout(function() {
   var quer = document.getElementsByClassName('pclass')
@@ -71,7 +55,6 @@ setTimeout(function() {
   } }, 200);
 
 }
-
 
 //стили
 function styleForm(x, p) {
@@ -83,44 +66,47 @@ function styleForm(x, p) {
       p.style.height = 20 * x + "px";
       p.style.backgroundColor = "#c7240c";
       }
-
 }
 
 function sort() {
-  if (i < arrayTo.length) {
-    if (j < arrayTo.length - 1) {
-      if (arrayTo[j] > arrayTo[j + 1]) {
-        var max = arrayTo[j];
-        var min = arrayTo[j + 1];
-        arrayTo[j] = min;
-        arrayTo[j + 1] = max;
-        tegs[j].innerHTML = arrayTo[j];
 
-        styleForm(arrayTo[j], tegs[j]);
-        tegs[j + 1].innerHTML = arrayTo[j + 1];
-        styleForm(arrayTo[j+1], tegs[j+1]);
-        console.log(tegs[j+1]);
-        setTimeout(function() {
-          tegs[j].style.backgroundColor = '#49ccf9';
-          // tegs[j].style.transition = 'right 1s ease-out 0.5s;'
-          tegs[j-1].style.backgroundColor = '#49ccf9';
-        }, 200)
 
+    if (i < length) {
+      if (j < length - 1) {
+        if (tegs[j].innerHTML > tegs[j + 1].innerHTML) {
+
+          tegs[j].style.left = '10px';
+          tegs[j+1].style.left = '-10px';
+
+          setTimeout(function() {
+            tegs[j-1].style.left = '0px';
+            tegs[j].style.left = '0px';
+            xmax = tegs[j-1].innerHTML;
+            xmin = tegs[j].innerHTML;
+            tegs[j].innerHTML = xmax;
+            tegs[j-1].innerHTML = xmin;
+            styleForm(xmax, tegs[j]);
+            styleForm(xmin, tegs[j-1]);
+          },60);
+
+          tegs[j].style.backgroundColor = "#c7240c";
+          tegs[j+1].style.backgroundColor = "#c7240c";
+
+          setTimeout(function() {
+            tegs[j].style.backgroundColor = '#49ccf9';
+            tegs[j-1].style.backgroundColor = '#49ccf9';
+          }, 200);
+
+        }
+        j++;
       }
-      j++;
+
+    }else{
+      i = 0;
+      j = 0;
     }
-
-  }else{
-    i = 0;
-    j = 0;
+    i++;
+  if (i == length){
+    length-=1;
   }
-  i++;
 }
-
-
-
-
-
-
-
-//=======
